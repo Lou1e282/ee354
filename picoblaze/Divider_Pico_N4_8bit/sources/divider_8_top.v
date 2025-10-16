6200b3c9fce6632ac45198190ecddd58d2d13f63
Lou1e282 <louieshe@usc.edu>
2025-10-15 21:18:44 -0700

    1

--- picoblaze/Divider_Pico_N4_8bit/sources/divider_8_top.v
+++ picoblaze/Divider_Pico_N4_8bit/sources/divider_8_top.v
@@ -31,7 +31,7 @@ module divider_8_top		(
 	input		ClkPort;	
 	// Project Specific Inputs
 	input		BtnL, BtnU, BtnD, BtnR, BtnC;	
-	input		Sw7, Sw6, Sw5, Sw4, Sw3, Sw2, Sw1, Sw0;
+	input		Sw15, Sw14, Sw13, Sw12, Sw11, Sw10, Sw9, Sw8, Sw7, Sw6, Sw5, Sw4, Sw3, Sw2, Sw1, Sw0;             // 16 inputs
 //*********TODO*************
 // You want to use all 16 switches here in the 8-bit divider top	
 	
@@ -52,11 +52,11 @@ module divider_8_top		(
 // *********TODO*************
 // You want to use all 8 SSDs. So you need to control 8 Anodes!
 // You may want to take a look at test_nexys4_verilog.v	
-	wire [1:0] 	ssdscan_clk;
+	wire [2:0] 	ssdscan_clk;  // 3 bit clk
 // *********TODO*************
 // Adjust the bit ranges	
-	wire [3:0] 	Xin, Yin;
-	reg  [3:0] 	Quotient, Remainder;
+	wire [7:0] 	Xin, Yin;
+	reg  [7:0] 	Quotient, Remainder;
 	wire 		Start, Ack;
 	reg 		Done, Qi, Qc, Qd;
 
@@ -66,7 +66,7 @@ module divider_8_top		(
 	reg [3:0]	SSD;
 // *********TODO*************
 // You want to use all 8 SSDs.	
-	wire [3:0]	SSD3, SSD2, SSD1, SSD0;
+	wire [7:0]	SSD7, SSD6, SSD5, SSD4,  SSD3, SSD2, SSD1, SSD0;
 	reg [7:0]  	SSD_CATHODES;
 
 
@@ -178,8 +178,8 @@ module divider_8_top		(
 
 // *********TODO*************
 // You want to use all 16 switches here in the 8-bit divider top	
-	assign Xin = {Sw7, Sw6, Sw5, Sw4};
-	assign Yin = {Sw3, Sw2, Sw1, Sw0};
+	assign Xin = {Sw15, Sw14, Sw13, Sw12, Sw11, Sw10, Sw9, Sw8,};
+	assign Yin = {Sw7, Sw6, Sw5, Sw4, Sw3, Sw2, Sw1, Sw0};
 	assign Start = BtnL; assign Ack = BtnR;
 
 // *********TODO*************
@@ -188,9 +188,10 @@ module divider_8_top		(
 // You may need 3 read ports with three port addresses	
 always @ (*)
 begin
-	case (port_id[0])
-		1'b0 : in_port <= {Xin,Yin};
-		1'b1 : in_port <= {6'b000000,Start,Ack}; 	
+	case (port_id[1:0])  
+		2'b00 : in_port <= {Xin};
+		2'b01 : in_port <= {Yin};
+		2'b11 : in_port <= {6'b000000,Start,Ack}; 	    // 8bit start ack 
 		default : in_port <= 8'bXXXXXXXX ;  
 	endcase
 end	
@@ -205,7 +206,7 @@ begin
 	// 'write_strobe' is used to qualify all writes to general output ports using OUTPUT.
 	if (write_strobe == 1'b1) 
 	begin
-		if(port_id[0] == 1'b0)begin
+		if(port_id[1] == 1'b0)begin               // 
 			{Quotient,Remainder} <= out_port;
 		end	
 	end
@@ -214,7 +215,7 @@ begin
 	if (k_write_strobe == 1'b1) 
 	begin
 		// Write to output_port at port address 01
-		if (port_id[0]  == 1'b1) 
+		if (port_id[1:0]  == 2'b11) 
 		begin
 			Done <= out_port[0];
 			Qi <= out_port[1];
@@ -240,6 +241,10 @@ end
 	// wire [3:0]	SSD3, SSD2, SSD1, SSD0;
 	
 	//SSDs display Xin, Yin, Quotient, and Remainder  
+	assign SSD7 =
+	assign SSD6 =
+	assign SSD5 =
+	assign SSD4 = 
 	assign SSD3 = Xin;
 	assign SSD2 = Yin;
 	assign SSD1 = Quotient;
@@ -270,22 +275,30 @@ end
 // You want to use all 8 SSDs. Hence you need to control 8 Anodes.
 // You may want to take a look at test_nexys4_verilog.v	
 
-	assign ssdscan_clk = DIV_CLK[19:18];
-	assign An0	= !(~(ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 00
-	assign An1	= !(~(ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 01
-	assign An2	=  !((ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 10
-	assign An3	=  !((ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 11
-	// Turn off another 4 anodes
-	assign {An7, An6, An5, An4} = 4'b1111;
+	assign ssdscan_clk = DIV_CLK[19:17];   // 
+	assign An0	= !(~(ssdscan_clk[2]) && ~(ssdscan_clk[1]) && ~(ssdscan_clk[0]) && );  // when ssdscan_clk = 000
+	assign An1	= !(~(ssdscan_clk[2]) && ~(ssdscan_clk[1]) && (ssdscan_clk[0]) && );  // when ssdscan_clk = 001
+	assign An2	=  !(~(ssdscan_clk[2]) && (ssdscan_clk[1]) && ~(ssdscan_clk[0]) && );  // when ssdscan_clk = 010
+	assign An3	=  !(~(ssdscan_clk[2]) && (ssdscan_clk[1]) && (ssdscan_clk[0]) && );  // when ssdscan_clk = 011
+	assign An4	= !((ssdscan_clk[2]) && ~(ssdscan_clk[1]) && ~(ssdscan_clk[0]) && );  // when ssdscan_clk = 100
+	assign An5	= !((ssdscan_clk[2]) && ~(ssdscan_clk[1]) && (ssdscan_clk[0]) && );  // when ssdscan_clk = 101
+	assign An6	=  !((ssdscan_clk[2]) && (ssdscan_clk[1]) && ~(ssdscan_clk[0]) && );  // when ssdscan_clk = 110
+	assign An7	=  !((ssdscan_clk[2]) && (ssdscan_clk[1]) && (ssdscan_clk[0]) && );  // when ssdscan_clk = 111
+	// Turn on another 4 anodes
+	// assign {An7, An6, An5, An4} = 4'b1111;
 	
 	
-	always @ (ssdscan_clk, SSD0, SSD1, SSD2, SSD3)
+	always @ (ssdscan_clk, SSD0, SSD1, SSD2, SSD3, SSD4, SSD5, SSD6, SSD7)
 	begin : SSD_SCAN_OUT
 		case (ssdscan_clk) 
-				  2'b00: SSD = SSD0;
-				  2'b01: SSD = SSD1;
-				  2'b10: SSD = SSD2;
-				  2'b11: SSD = SSD3;
+				  3'b000: SSD = SSD0;
+				  3'b001: SSD = SSD1;
+				  3'b010: SSD = SSD2;
+				  3'b011: SSD = SSD3;
+				  3'b100: SSD = SSD4;
+				  3'b101: SSD = SSD5;
+				  3'b110: SSD = SSD6;
+				  3'b111: SSD = SSD7;
 		endcase 
 	end
 
@@ -297,22 +310,22 @@ end
 	begin : HEX_TO_SSD
 		case (SSD) // in this solution file the dot points are made to glow by making Dp = 0
 		    //                                                                abcdefg,Dp
-			4'b0000: SSD_CATHODES = 8'b00000010; // 0
-			4'b0001: SSD_CATHODES = 8'b10011110; // 1
-			4'b0010: SSD_CATHODES = 8'b00100100; // 2
-			4'b0011: SSD_CATHODES = 8'b00001100; // 3
-			4'b0100: SSD_CATHODES = 8'b10011000; // 4
-			4'b0101: SSD_CATHODES = 8'b01001000; // 5
-			4'b0110: SSD_CATHODES = 8'b01000000; // 6
-			4'b0111: SSD_CATHODES = 8'b00011110; // 7
-			4'b1000: SSD_CATHODES = 8'b00000000; // 8
-			4'b1001: SSD_CATHODES = 8'b00001000; // 9
-			4'b1010: SSD_CATHODES = 8'b00010000; // A
-			4'b1011: SSD_CATHODES = 8'b11000000; // B
-			4'b1100: SSD_CATHODES = 8'b01100010; // C
-			4'b1101: SSD_CATHODES = 8'b10000100; // D
-			4'b1110: SSD_CATHODES = 8'b01100000; // E
-			4'b1111: SSD_CATHODES = 8'b01110000; // F    
+			4'b0000: SSD_CATHODES = 8'b00000011; // 0
+			4'b0001: SSD_CATHODES = 8'b10011111; // 1
+			4'b0010: SSD_CATHODES = 8'b00100101; // 2
+			4'b0011: SSD_CATHODES = 8'b00001101; // 3
+			4'b0100: SSD_CATHODES = 8'b10011001; // 4
+			4'b0101: SSD_CATHODES = 8'b01001001; // 5
+			4'b0110: SSD_CATHODES = 8'b01000001; // 6
+			4'b0111: SSD_CATHODES = 8'b00011111; // 7
+			4'b1000: SSD_CATHODES = 8'b00000001; // 8
+			4'b1001: SSD_CATHODES = 8'b00001001; // 9
+			4'b1010: SSD_CATHODES = 8'b00010001; // A
+			4'b1011: SSD_CATHODES = 8'b11000001; // B
+			4'b1100: SSD_CATHODES = 8'b01100011; // C
+			4'b1101: SSD_CATHODES = 8'b10000101; // D
+			4'b1110: SSD_CATHODES = 8'b01100001; // E
+			4'b1111: SSD_CATHODES = 8'b01110001; // F    
 			default: SSD_CATHODES = 8'bXXXXXXXX; // default is not needed as we covered all cases
 		endcase
 	end	
